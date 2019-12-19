a82642e81a563c22f087b70ea3e44f856559e1c0
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2019-12-19 22:41:01 +0100

    Create C - Alchemist.py

---
+++ AtCoder/ABC138/C - Alchemist.py
@@ -0,0 +1,11 @@
+#https://atcoder.jp/contests/abc138/tasks/abc138_c
+
+n=int(input())
+v=list(map(int,input().split()))
+v.sort()
+while len(v)>1:
+    a=v.pop(0)
+    b=v.pop(0)
+    v.append((a+b)/2)
+    v.sort()
+print(v[0])
