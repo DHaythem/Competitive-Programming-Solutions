f7b4be88f410015fa24ccd5339acd7c125f3126f
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2019-12-21 16:59:55 +0100

    Create B - Ordinary Number.py

---
+++ AtCoder/ABC132/B - Ordinary Number.py
@@ -0,0 +1,9 @@
+#https://atcoder.jp/contests/abc132/tasks/abc132_b
+
+n=int(input())
+l=list(map(int,input().split()))
+s=0
+for i in range(1,n-1):
+    if (l[i]>l[i-1] and l[i]<l[i+1]) or (l[i]<l[i-1] and l[i]>l[i+1]):
+        s+=1
+print(s)
