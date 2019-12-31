9b3f844d027fc9f45a2f61d013d7aa633614b0ce
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2019-12-31 18:51:00 +0100

    Create A - Still TBD.py

---
+++ AtCoder/ABC119/A - Still TBD.py
@@ -0,0 +1,12 @@
+#https://atcoder.jp/contests/abc119/tasks/abc119_a
+
+s=input()
+y=int(s[:4])
+m=int(s[5:7])
+d=int(s[8:])
+if y>2019:
+    print('TBD')
+elif m>4:
+    print('TBD')
+else:
+    print('Heisei')
