619dc458a1550393876e302a6d501b385f617477
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-03-30 14:50:02 +0200

    Create A. Mahmoud and Ehab and the even-odd game.cpp

---
+++ Codeforces/600/A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/problemset/problem/959/A
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);   cout.precision(30);
+using namespace std;
+
+int main()
+{
+    IOS;
+    int n; cin >> n;
+    cout << (n%2 == 0 ? "Mahmoud" : "Ehab");
+}
