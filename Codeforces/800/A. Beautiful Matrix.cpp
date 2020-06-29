69d247ff59fb71cdf0f9c7522f36752493caa1d4
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-06-29 21:50:53 +0200

    Create A. Beautiful Matrix.cpp

---
+++ Codeforces/800/A. Beautiful Matrix.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+#define ll long long
+#define pi 3.14159265359
+using namespace std;
+
+int main()
+{
+    IOS;
+
+    int a,x=0,y=1;
+    for (int i = 0; i<25; i++){
+
+        cin>>a;
+        x++;
+
+        if (a==1){
+            cout<<abs(x-3)+abs(y-3)<<"\n";
+            break;
+        }
+
+        if (x==5){
+            y++;
+            x=0;
+        }
+    }
+    return 0;
+}
