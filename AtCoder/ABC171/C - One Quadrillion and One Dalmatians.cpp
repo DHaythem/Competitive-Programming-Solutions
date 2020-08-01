cb6964728e28179cf1adfcc616a194814dda0913
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-08-01 20:02:27 +0200

    Create C - One Quadrillion and One Dalmatians.cpp

---
+++ AtCoder/ABC171/C - One Quadrillion and One Dalmatians.cpp
@@ -0,0 +1,21 @@
+//https://atcoder.jp/contests/abc171/tasks/abc171_c
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+#define ll long long
+#define pi 3.14159265359
+using namespace std;
+
+int main()
+{
+    IOS;
+    int n;
+    cin>>n;
+    string s;
+    while(n--){
+        s=char(97+n%26)+s;
+        n/=26;
+    }
+    cout<<s;
+    return 0;
+}
