31252cbdc29d25c7bc7abe2040b28db8006c6f62
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-08-01 18:10:33 +0200

    Create A - αlphabet.cpp

---
+++ AtCoder/ABC171/A - αlphabet.cpp
@@ -0,0 +1,17 @@
+//https://atcoder.jp/contests/abc171/tasks/abc171_a
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+#define ll long long
+#define pi 3.14159265359
+using namespace std;
+
+int main()
+{
+    IOS;
+    char a;
+    cin>>a;
+    if(isupper(a)) cout<<'A';
+    else cout<<'a';
+    return 0;
+}
