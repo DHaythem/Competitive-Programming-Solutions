42ed6423c835165a097f943ee9dffcf1f8b2adde
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-08-06 20:24:43 +0200

    Create Multiplication 1.cpp

---
+++ AtCoder/ABC169/Multiplication 1.cpp
@@ -0,0 +1,16 @@
+//https://atcoder.jp/contests/abc169/tasks/abc169_a
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+#define ll long long
+#define pi 3.14159265359
+using namespace std;
+
+int main()
+{
+    IOS;
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b;
+    return 0;
+}
