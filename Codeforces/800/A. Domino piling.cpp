0df2cc56b8fa753864b2db54d595fae5dca09635
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-10-06 19:56:07 +0200

    Create A. Domino piling.cpp

---
+++ Codeforces/800/A. Domino piling.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/50/A
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+using namespace std;
+
+int main()
+{
+    IOS;
+    int m,n;
+    cin>>m>>n;
+    cout<<m*n/2;
+    return 0;
+}
