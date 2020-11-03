d9b8e54812f40b6e8622084d1f4b2d895f454969
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-11-03 18:06:21 +0100

    Create C - Collinearity.cpp

---
+++ AtCoder/ABC181/C - Collinearity.cpp
@@ -0,0 +1,32 @@
+//https://atcoder.jp/contests/abc181/tasks/abc181_c
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+#define ll long long
+#define ld long double
+using namespace std;
+
+int main()
+{
+    IOS;
+    ll n,s,f=1;
+    cin>>n;
+    ll x[n],y[n];
+    for(int i=0;i<n;i++) cin>>x[i]>>y[i];
+    for(int i=0;i<n-2;i++){
+        for(int j=i+1;j<n-1;j++){
+            for(int k=j+1;k<n;k++){
+                s=x[i]*(y[j]-y[k])+x[j]*(y[k]-y[i])+x[k]*(y[i]-y[j]);
+                if(s==0){
+                    cout<<"Yes";
+                    f=0;
+                    break;
+                }
+            }
+            if(f==0) break;
+        }
+        if(f==0) break;
+    }
+    if(f) cout<<"No";
+    return 0;
+}
