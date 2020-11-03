1ab1765684ea50727520906b67941b11061e440e
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-11-03 18:11:55 +0100

    Create A - box.cpp

---
+++ AtCoder/ABC180/A - box.cpp
@@ -0,0 +1,16 @@
+//https://atcoder.jp/contests/abc180/tasks/abc180_a
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+#define ll long long
+#define ld long double
+using namespace std;
+
+int main()
+{
+    IOS;
+    int n,a,b;
+    cin>>n>>a>>b;
+    cout<<n-a+b;
+    return 0;
+}
