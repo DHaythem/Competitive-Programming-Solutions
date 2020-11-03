3ed2fef62d3a10227c073671ab8e847c3fb984b9
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-11-03 17:29:31 +0100

    Create A - Heavy Rotation.cpp

---
+++ AtCoder/ABC181/A - Heavy Rotation.cpp
@@ -0,0 +1,17 @@
+//https://atcoder.jp/contests/abc181/tasks/abc181_a
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+#define ll long long
+#define ld long double
+using namespace std;
+
+int main()
+{
+    IOS;
+    int n;
+    cin>>n;
+    if(n%2==0) cout<<"White";
+    else cout<<"Black";
+    return 0;
+}
