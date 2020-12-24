0c5c083bd3619c8ef8b0b6a045f17328b08fbdf3
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-12-24 20:54:54 +0100

    Create A - Strings.cpp

---
+++ AtCoder/ABC149/A - Strings.cpp
@@ -0,0 +1,17 @@
+//https://atcoder.jp/contests/abc149/tasks/abc149_a
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(30);
+using namespace std;
+typedef long long ll;
+#define pi 3.14159265
+
+int main()
+{
+    //freopen("circle.in", "r", stdin);
+    IOS;
+    string s,t;
+    cin>>s>>t;
+    cout<<t<<s;
+    return 0;
+}
