71f9cf12ed68cc14feb39bbe00a4cb50c2ebd462
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2021-01-16 16:33:23 +0100

    Create A. Alex and a Rhombus.cpp

---
+++ Codeforces/800/A. Alex and a Rhombus.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/problemset/problem/1180/A
+
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll; //up to 10^18
+
+
+int main()
+{
+	IOS;
+    int n;
+    cin>>n;
+    cout<<1+2*n*n-2*n;
+    return 0;
+}
