edc0374928c0d6ecc35e0839b6b84a52a550273f
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2021-01-17 11:32:19 +0100

    Create A. Filling Diamonds.cpp

---
+++ Codeforces/900/A. Filling Diamonds.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/problemset/problem/1339/A
+
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main()
+{
+	IOS;
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<n<<'\n';
+    }
+        return 0;
+}
